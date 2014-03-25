3bfc39603a736836230509f8bcbb46ccb017566b
杯面小王子 <xu_jianye@qq.com>
2014-03-25 17:17:05 +0800

    detect scroll

--- What's Weather/WeatherViewController.h
+++ What's Weather/WeatherViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "BTGlassScrollView.h"
-@interface WeatherViewController : UIViewController <UIScrollViewAccessibilityDelegate>
+#import "ImgCell.h"
+@interface WeatherViewController : UIViewController <UICollectionViewDelegate>
 
 @end
--- What's Weather/WeatherViewController.m
+++ What's Weather/WeatherViewController.m
@@ -14,27 +14,19 @@
 @interface WeatherViewController ()
 @property (weak, nonatomic) IBOutlet UIView *topbarView;
 @property (weak, nonatomic) IBOutlet UILabel *topbarTitle;
-
+@property (strong,nonatomic) NSMutableArray *imgArray;
+@property (strong,nonatomic) UICollectionViewTransitionLayout *transitionLayout;
+@property (weak, nonatomic) IBOutlet UICollectionView *locationCollection;
 @end
 
 @implementation WeatherViewController
-{
-    BTGlassScrollView *_glassScrollView;
-    
-    UIScrollView *_viewScroller;
-    BTGlassScrollView *_glassScrollView1;
-    BTGlassScrollView *_glassScrollView2;
-    BTGlassScrollView *_glassScrollView3;
-    int _page;
-    
-}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        _page = 0;
+
     }
     
     return self;
@@ -42,134 +34,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    //showing white status
-    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
-    
-    
-    //preventing weird inset
-    if([self respondsToSelector:@selector(setAutomaticallyAdjustsScrollViewInsets:)]){
-        self.automaticallyAdjustsScrollViewInsets = NO;
-    }
-    
-    //background
-    self.view.backgroundColor = [UIColor blackColor];
-    
-    if (SIMPLE_SAMPLE) {
-        //create your custom info views
-        //UIView *view = [self customView];
-        
-        _glassScrollView = [[BTGlassScrollView alloc] initWithFrame:self.view.frame BackgroundImage:[UIImage imageNamed:@"background3"] blurredImage:nil viewDistanceFromBottom:120 foregroundView:nil];
-        
-        [self.view addSubview:_glassScrollView];
-    }else{
-        CGFloat blackSideBarWidth = 2;
-        
-        _viewScroller = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width + 2*blackSideBarWidth, self.view.frame.size.height)];
-        [_viewScroller setPagingEnabled:YES];
-        [_viewScroller setDelegate:self];
-        [_viewScroller setShowsHorizontalScrollIndicator:NO];
-        [self.view addSubview:_viewScroller];
-        
-        _glassScrollView1 = [[BTGlassScrollView alloc] initWithFrame:self.view.frame BackgroundImage:[UIImage imageNamed:@"yunnan.jpg"] blurredImage:nil viewDistanceFromBottom:120 foregroundView:nil];
-        _glassScrollView2 = [[BTGlassScrollView alloc] initWithFrame:self.view.frame BackgroundImage:[UIImage imageNamed:@"shanghai.jpg"] blurredImage:nil viewDistanceFromBottom:120 foregroundView:nil];
-        _glassScrollView3 = [[BTGlassScrollView alloc] initWithFrame:self.view.frame BackgroundImage:[UIImage imageNamed:@"daocheng.jpg"] blurredImage:nil viewDistanceFromBottom:120 foregroundView:nil];
-        
-        [_viewScroller addSubview:_glassScrollView1];
-        [_viewScroller addSubview:_glassScrollView2];
-        [_viewScroller addSubview:_glassScrollView3];
-    }
     [self addTopbarIco];
     [self.view bringSubviewToFront:self.topbarView];
-    
-}
-- (void)viewWillAppear:(BOOL)animated
-{
-    if (!SIMPLE_SAMPLE) {
-        int page = _page; // resize scrollview can cause setContentOffset off for no reason and screw things up
-        
-        CGFloat blackSideBarWidth = 2;
-        [_viewScroller setFrame:CGRectMake(0, 0, self.view.frame.size.width + 2*blackSideBarWidth, self.view.frame.size.height)];
-        [_viewScroller setContentSize:CGSizeMake(3*_viewScroller.frame.size.width, self.view.frame.size.height)];
-        
-        [_glassScrollView1 setFrame:self.view.frame];
-        [_glassScrollView2 setFrame:self.view.frame];
-        [_glassScrollView3 setFrame:self.view.frame];
-        
-        [_glassScrollView2 setFrame:CGRectOffset(_glassScrollView2.bounds, _viewScroller.frame.size.width, 0)];
-        [_glassScrollView3 setFrame:CGRectOffset(_glassScrollView3.bounds, 2*_viewScroller.frame.size.width, 0)];
-        
-        [_viewScroller setContentOffset:CGPointMake(page * _viewScroller.frame.size.width, _viewScroller.contentOffset.y)];
-        _page = page;
-    }
-    
-    //show animation trick
-    double delayInSeconds = 2.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        // [_glassScrollView1 setBackgroundImage:[UIImage imageNamed:@"background"] overWriteBlur:YES animated:YES duration:1];
-    });
-}
-
-- (void)viewWillLayoutSubviews
-{
-    // if the view has navigation bar, this is a great place to realign the top part to allow navigation controller
-    // or even the status bar
-    /*
-    if (SIMPLE_SAMPLE) {
-        [_glassScrollView setTopLayoutGuideLength:[self.topLayoutGuide length]];
-    }else{
-        [_glassScrollView1 setTopLayoutGuideLength:[self.topLayoutGuide length]];
-        [_glassScrollView2 setTopLayoutGuideLength:[self.topLayoutGuide length]];
-        [_glassScrollView3 setTopLayoutGuideLength:[self.topLayoutGuide length]];
-    }
-     */
-}
-
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    CGFloat ratio = scrollView.contentOffset.x/scrollView.frame.size.width;
-    _page = (int)floor(ratio);
-    NSLog(@"%i",_page);
-    if (ratio > -1 && ratio < 1) {
-        [_glassScrollView1 scrollHorizontalRatio:-ratio];
-    }
-    if (ratio > 0 && ratio < 2) {
-        [_glassScrollView2 scrollHorizontalRatio:-ratio + 1];
-    }
-    if (ratio > 1 && ratio < 3) {
-        [_glassScrollView3 scrollHorizontalRatio:-ratio + 2];
-    }
-}
-
-- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView
-{
-    BTGlassScrollView *glass = [self currentGlass];
-    NSLog(@"begin drag");
-    //can probably be optimized better than this
-    //this is just a demonstration without optimization
-    
-    [_glassScrollView1 scrollVerticallyToOffset:glass.foregroundScrollView.contentOffset.y];
-    [_glassScrollView2 scrollVerticallyToOffset:glass.foregroundScrollView.contentOffset.y];
-    [_glassScrollView3 scrollVerticallyToOffset:glass.foregroundScrollView.contentOffset.y];
-}
-
-- (BTGlassScrollView *)currentGlass
-{
-    BTGlassScrollView *glass;
-    switch (_page) {
-        case 0:
-            glass = _glassScrollView1;
-            break;
-        case 1:
-            glass = _glassScrollView2;
-            break;
-        case 2:
-            glass = _glassScrollView3;
-        default:
-            break;
-    }
-    return glass;
+    self.imgArray=[[NSMutableArray alloc]initWithArray:@[[UIImage imageNamed:@"yunnan.jpg"],[UIImage imageNamed:@"daocheng.jpg"],[UIImage imageNamed:@"shanghai.jpg"]]];
 }
 
 - (BOOL)shouldAutorotate
@@ -192,6 +59,16 @@
     }
     
 }
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section{
+    return [self.imgArray count];
+}
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    // Register nib file for the cell
+    ImgCell *imgCell=[cv dequeueReusableCellWithReuseIdentifier:@"imgCell" forIndexPath:indexPath];
+    imgCell.imageView.image =[self.imgArray objectAtIndex:indexPath.row];
+    return imgCell;
+
+}
 -(void)addTopbarIco{
     //待优化
     NSString *str=self.topbarTitle.text;
@@ -202,5 +79,9 @@
 
     topbar_icoLocation.frame=CGRectMake(self.topbarTitle.center.x-strWidth/2-20,14,20,20);
     [self.topbarView addSubview:topbar_icoLocation];
+}
+
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    NSIndexPath *indexPath=[self.locationCollection indexPathsForVisibleItems].firstObject;
 }
 @end
